1e1531ae7f951aacb531ec4038694bab3932865f
franciscodelgadodp <franciscodelgadodp@gmail.com>
2023-07-13 17:41:59 -0600

    - Added data fetching from Firebase and modified logic to adapt to these changes - Added new route to display a specific Category Component and added CategoryPreview component - Changed name of ProductContext to categories context to improve readability

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,7 +8,17 @@ import {
   signOut,
   onAuthStateChanged
  } from  'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { 
+  getFirestore, 
+  doc, 
+  getDoc, 
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs
+} from 'firebase/firestore';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAQpyhz8OTHpbYL33Y_sOYuBq94RSP07E0",
@@ -68,3 +78,30 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+
+export const addCollectionsAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach(object => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done')
+};
+
+export const getCategoriesAndDocuments = async () => {
+   const collectionRef = collection(db, 'categories');
+   const q = query(collectionRef);
+
+   const querySnapshot = await getDocs(q);
+   const categoryMap = querySnapshot.docs.reduce((accumulator, docSnapShot) => {
+    const { title, items } = docSnapShot.data();
+    accumulator[title.toLowerCase()] = items;
+    return accumulator;
+   }, {});
+
+   return categoryMap;
+};
