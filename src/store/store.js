2ba1551ac002fc20f16e126f21cd656a840b8d30
franciscodelgadodp <franciscodelgadodp@gmail.com>
2023-07-18 15:47:46 -0600

    - implement redux-persist in the application

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducers';
@@ -7,4 +10,14 @@ const middlewares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+  key: 'root',
+  storage,
+  blackList: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store)
